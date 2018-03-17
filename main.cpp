007cca847905583eb2a3cea00957f71f3e2d1a48
hangshen <shenhang105@gamil.com>
2018-03-17 15:28:41 +0800

    add GPU

--- main.cpp
+++ main.cpp
@@ -14,13 +14,13 @@ int main(int argc, const char * argv[]) {
     /* Parameters */
     int n, r, q, k, s;
     float *data, *queries;
-    n = atoi( argv[1] ); // Size of dataset (rows
-    r = atoi( argv[2] ); // Number of dimensions (columns)
-    k = atoi( argv[3] ); // Nearest neighbors
-    q = atoi( argv[4] ); // Number of queries
+    n = 247753; // Size of dataset (rows
+    r = 50; // Number of dimensions (columns)
+    k = 100000; // Nearest neighbors
+    q = 1; // Number of queries
 
-    string inputFile = argv[6];
-    string queryFile = argv[7];
+    string inputFile = "/Users/hangshen/Desktop/data_ming/recommdation/MFRetrieval-master/MovieLens/q.txt";
+    string queryFile = "/Users/hangshen/Desktop/data_ming/recommdation/MFRetrieval-master/MovieLens/p2.txt";
 
     data = (float *)malloc((int64_t)sizeof(float)*n*r); check_alloc(data);
     queries = (float *)malloc((int64_t)sizeof(float)*q*r); check_alloc(queries);
@@ -35,27 +35,30 @@ int main(int argc, const char * argv[]) {
     cout<<"finsh reading data"<<endl;
 
     std::cout << "Data size:  " << (sizeof(float)*n*r)/(1024*1024) << "MB (" << n << " x " << r << ")" << std::endl;
-    std::cout << "Query size: " << (sizeof(float)*q*r)/(1024*1024) << "MB (" << q << " x " << r << ")" << std::endl;
+    std::cout << "Query size: " << (sizeof(float)*q*r)/(1024*1024) << "MB (" << q << " x p" << r << ")" << std::endl;
     std::cout << "Dist. size: " << (sizeof(float)*n*q)/(1024*1024) << "MB (" << n << " x " << q << ")" << std::endl;
     std::cout << "Index size: " << (sizeof(float)*k*q)/(1024*1024) << "MB (" << k << " x " << q << ")" << std::endl;
     std::cout << "Total:      " << ((sizeof(float)*n*r) + (sizeof(float)*q*r) + (sizeof(float)*n*q) + (sizeof(float)*k*q))/(1024*1024) << "MB" << std::endl;
 
+    float* dataSample = (float *)malloc(sizeof(float)*s*r); check_alloc(dataSample);
+    float* querySample = (float *)malloc(sizeof(float)*s*r); check_alloc(dataSample);
     float* bdistances = (float *)malloc((int64_t)sizeof(float)*n*q); check_alloc(bdistances);
     int *knn = (int *)malloc(sizeof(int)*k*q); check_alloc(knn);
 
     clock_t timeStart2 = clock();
     knn_distance(&data, &bdistances, n, r, k, &queries, q);
     clock_t timeEnd2 = clock();
-    double processTime = (timeEnd2-timeStart2)/CLOCKS_PER_SECOND ;
+    double processTime = (timeEnd2-timeStart2)/CLOCKS_PER_SECOND*1000.0 ;
     cout<<"Duration of process time: "<< processTime <<endl;
 
     clock_t timeStart3 = clock();
     mksort(&bdistances, &thresholds, n, q, k);
     clock_t timeEnd3 = clock();
-    double sortTime = (timeEnd3-timeStart3)/CLOCKS_PER_SECOND ;
+    double sortTime = (timeEnd3-timeStart3)/CLOCKS_PER_SECOND*1000.0 ;
     cout<<"Duration of sort time: "<< sortTime <<endl;
 
     free(bdistances); bdistances = nullptr;
+    free(knn); dataSample = nullptr;
     free(data); data = nullptr;
     free(queries); queries = nullptr;
 
---
+++ matrixMulCPU.h
@@ -0,0 +1,8 @@
+//
+// Created by Hang Shen on 2018/3/17.
+//
+
+#ifndef BASELINE_MATRIXMULCPU_H
+#define BASELINE_MATRIXMULCPU_H
+//int matrixMultiply(int argc, char **argv, int block_size, dim3 &dimsA, dim3 &dimsB);
+#endif //BASELINE_MATRIXMULCPU_H
